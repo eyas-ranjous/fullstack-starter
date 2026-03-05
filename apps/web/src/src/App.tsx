2ed8f19af44494314c505b3a1c26002bf3280cda
eyas-ranjous <eyas.ranjous@gmail.com>
2026-03-04 22:13:26 -0800

    pnpm

---
+++ apps/web/src/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40, fontFamily: "system-ui" }}>
+      <h1>Fullstack Starter</h1>
+      <p>React app is running.</p>
+    </div>
+  );
+}
